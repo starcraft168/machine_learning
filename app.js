099b321c124d1ccf6442926d643d00794365fbcd
starcraft168 <ericeng168@gmail.com>
2017-02-05 21:44:37 -0800

    initialise params

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ h(x) = a0 + a1*x
 Let alpha be the learning rate = 0.1
 Let params = [-20,10], a random vector of parameters
 Let input_array = [[2104, 460], [1416,232], [1534, 315], [852,178]]
-Let m be the number of training samples.
+Let m be the number of training samples
 
 
 Goal is to continuously train the sample until the parameters converge.
@@ -17,6 +17,7 @@ Goal is to continuously train the sample until the parameters converge.
 
 var gradientDescent = function(input_array, alpha, params) {
 	var m = input_array.length;
-	
+	var a0 = params[0];
+	var a1 = params[1];
 
 }
