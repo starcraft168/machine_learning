8ab9e38c50725b6d2d507046df8731c7f7005f37
starcraft168 <ericeng168@gmail.com>
2017-02-05 22:01:10 -0800

    progress

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-Assumes this is a linear regression gradient descent function
+Assumes this is a linear regression gradient descent algorithm
 
 Assumes the input_array is a training sample with one feature (not including bias term) and y term,
 ie, [[2104, 460], [1416,232], [1534, 315], [852,178]], where [x,y] = [house sq ft, house price]
@@ -19,5 +19,25 @@ var gradientDescent = function(input_array, alpha, params) {
 	var m = input_array.length;
 	var a0 = params[0];
 	var a1 = params[1];
+
+	// a1 = a1 - alpha*(1/m)*()
+
+	var sum = 0;
+	var cost_a0 = 0;
+	var cost_a1 = 0;
+	//iterate through the training sample 
+	for(var i=0; i<m; i++) {
+		var xi = input_array[i][0];
+		var yi = input_array[i][1];
+		var cost_a0 = (1/m)*(a0+a1*xi-yi)
+		var cost_a1 = (1/m)*(a0+a1*xi-yi)*xi
+		a0 = a0 - alpha*cost_a0;
+		a1 = a1 - alpha*cost_a1;
+		// sum += 
+	}
+
+}
+
+var updateParams = function() {
 
 }
