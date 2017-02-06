094e36b5e72190ed5c4b75af6c26d9fd373c4262
starcraft168 <ericeng168@gmail.com>
2017-02-05 21:41:31 -0800

    define assumptions

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var gradientDescent = function() {
+/*
+Assumes this is a linear regression gradient descent function
 
-};
+Assumes the input_array is a training sample with one feature (not including bias term) and y term,
+ie, [[2104, 460], [2104,460], [1416,232], [1534, 315], [852,178]], where [x,y] = [house sq ft, house price]
+
+h(x) = a0 + a1*x
+
+Let alpha be the learning rate.
+Let 
+
+
+
+Goal is to train the sample to determine the gradient
+*/
+
+var gradientDescent = function(input_array, alpha) {
+
+
+}
