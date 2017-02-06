b44730b1a79b0607fcfc31707ec050a4d31cd6a5
starcraft168 <ericeng168@gmail.com>
2017-02-05 21:43:01 -0800

    define m

--- app.js
+++ app.js
@@ -2,19 +2,21 @@
 Assumes this is a linear regression gradient descent function
 
 Assumes the input_array is a training sample with one feature (not including bias term) and y term,
-ie, [[2104, 460], [2104,460], [1416,232], [1534, 315], [852,178]], where [x,y] = [house sq ft, house price]
+ie, [[2104, 460], [1416,232], [1534, 315], [852,178]], where [x,y] = [house sq ft, house price]
 
 h(x) = a0 + a1*x
 
-Let alpha be the learning rate.
-Let 
+Let alpha be the learning rate = 0.1
+Let params = [-20,10], a random vector of parameters
+Let input_array = [[2104, 460], [1416,232], [1534, 315], [852,178]]
+Let m be the number of training samples.
 
 
-
-Goal is to train the sample to determine the gradient
+Goal is to continuously train the sample until the parameters converge.
 */
 
-var gradientDescent = function(input_array, alpha) {
-
+var gradientDescent = function(input_array, alpha, params) {
+	var m = input_array.length;
+	
 
 }
